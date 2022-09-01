d96ca7f9b48e40b80235c7693f572f811e3f3ff4
JocelynCamero <jocelyncamero@gmail.com>
2022-08-31 21:08:34 -0500

    Se agrego el componente buscador funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "@mui/system";
 import Searcher from "./components/searcher/searcher";
 
 const App = () => {
+    
+    const [inputUser, setInputUser] = useState("octocat");
+    const [userState, setUserState] = useState("inputUser");
+    
+    // Estilo del componente Container
+    const containerStyles = {
+        background: 'whitesmoke',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      };
+
     return(
-        <Container sx={{
-            background: 'whitesmoke',
-            width: '80vw',
-            height: '500px',
-            borderRadius: '16px',
-            marginTop: '40px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-        }}>
-            <Searcher></Searcher>
+        <Container sx={containerStyles}>
+            <Searcher inputUser={inputUser} setInputUser={setInputUser} />
         </Container>
     )
 };
--- src/components/searcher/searcher.jsx
+++ src/components/searcher/searcher.jsx
@@ -1,32 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Stack, IconButton } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
-const Searcher = () => {
-    return(
-    <Stack
-    direction="row"
-    sx={{
+const Searcher = (props) => {
+
+    const {setInputUser} = props;
+    // Estado del valor del input
+    const [valueInput, setValueInput] = useState("");
+
+    // Funcion que guarda el valor ingresado en el input
+    const onSearchValueChange = (e) => {
+        const inputValue=e.target.value;
+        setValueInput(inputValue);
+        //console.log("Valor de cambio de tecla", inputValue);
+    }
+
+    // Funcion que envia el valor a buscar
+    const handleSubmit = () => {
+        setInputUser(valueInput);
+        // console.log("Valor que se envia a buscar", valueInput);
+    }
+
+    // Estilos del componente Stack
+    const stackStyles = {
         marginTop:"30px",
         width:"80%"
-    }}>
+    }
+
+    // Estilos del componente TextField
+    const textFieldStyles = {
+        width:"90%"
+    }
+
+    // Estilos del componente IconButton
+    const iconButtonStyles = {
+        left:"-45px"
+    }
+
+    return(
+    <Stack direction="row" sx={stackStyles}>
         <TextField
         id="outlined-basic"
         label="Github User"
         variant="outlined"
         placeholder="Buscar usurio Github"
         size="small"
-        sx={{
-            width:"90%"
-        }}
+        value={valueInput}
+        onChange={onSearchValueChange}
+        sx={textFieldStyles}
         />
         <IconButton
         size="small"
-        sx={{
-            left:"-45px"
-        }}
+        onClick={handleSubmit}
+        sx={iconButtonStyles}
         >
-            <SearchIcon/>
+        <SearchIcon/>
         </IconButton>
     </Stack>
 )
